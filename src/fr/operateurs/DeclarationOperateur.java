4f703b13ce09e58a38183c7ddcdd326a16685c2e
Mayran <loicmayran@gmail.com>
2024-10-02 14:33:13 +0200

    TP3 : Ajout package et class DeclarationOperateur ainsi que le test des opérateurs

---
+++ src/fr/operateurs/DeclarationOperateur.java
@@ -0,0 +1,13 @@
+package fr.operateurs;
+
+public class DeclarationOperateur {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        boolean testEt = a>0 && b<10;
+        System.out.println(testEt);
+
+        boolean testOu = a>0 || b<10;
+        System.out.println(testOu);
+    }
+}
