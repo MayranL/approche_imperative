5dd7b87290913c97d8aab269299ea00c26bd38ad
Mayran <loicmayran@gmail.com>
2024-10-02 14:12:10 +0200

    TP2 : Ajout class DeclarationApp et déclaration des variablesÃ

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,15 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        byte valByte = 0001;
+        short valShort = 10;
+        int valInt = 1;
+        long valLong = 1000;
+        float valFloat = 12.34F;
+        double valDouble = 12.34;
+        char valChar = 'A';
+        boolean valBoolean = true;
+        String valString = "Bonjour";
+    }
+}
