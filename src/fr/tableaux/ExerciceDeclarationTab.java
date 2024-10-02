29833ae5f0317e39d80f2dddaef660863836eb41
Mayran <loicmayran@gmail.com>
2024-10-02 14:58:22 +0200

    TP4 : Exercice declaration et modification

--- src/fr/tableaux/ExerciceDeclarationTab.java
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -2,6 +2,7 @@ package fr.tableaux;
 
 public class ExerciceDeclarationTab {
     public static void main(String[] args){
+        System.out.println("Exercice : Declaration de base\n");
         int array[] = {8,-7,12,1,-2,14,17,9};
         System.out.println("Index 0 : "+ array[0]);
 
---
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -0,0 +1,27 @@
+package fr.tableaux;
+
+import java.util.Arrays;
+
+public class ExerciceTableauChaines {
+    public static void main(String[] args){
+        System.out.println("\nExercice : Declaration et modification\n");
+        String array[] = {"Nantes","Toulouse","Montpellier","Paris","Albi"};
+
+        // Première option pour les afficher distinctements
+        for(int i =0; i<array.length;i++){
+            System.out.println("Element numéro "+i+" de la liste : "+array[i]);
+        }
+        // Deuxième option pour afficher toutes les valeurs dans le tableau
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("Taille du tableau : "+array.length);
+
+        array[3] = "Reims";
+        // Première option pour les afficher distinctements
+        for(int i =0; i<array.length;i++){
+            System.out.println("Element numéro "+i+" de la liste : "+array[i]);
+        }
+        // Deuxième option pour afficher toutes les valeurs dans le tableau
+        System.out.println(Arrays.toString(array));
+    }
+}
